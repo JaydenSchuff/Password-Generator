b5d2a6675b628cc1987f4e67258cd00a4bfeba10
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-03-21 12:24:02 -0600

    added variables

--- friendly-parakeet-main/friendly-parakeet-main/Develop/assets/script.js
+++ friendly-parakeet-main/friendly-parakeet-main/Develop/assets/script.js
@@ -1,9 +1,31 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+// Assignment code here
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var special = "!@#$%^&*()_-+=[]{}:;<>,.?/|~`"
+
+
+function generatePassword() {
+  var password = "";
+  var passwordText = "";
+  passwordlength = parseInt(passwordlength);
+
+  var passwordlength = prompt("Password must be between 8-128 characters!");
+
+  if (passwordlength < 8){
+    alert("Password must have more than 7 characters!");
+    return "";
+  }
+  else (passwordlength > 128){
+    alert("Password must have less than 128 characters!");
+    return"";
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
