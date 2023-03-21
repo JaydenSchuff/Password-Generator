d208d051c7f7de0ebc7397ab11e0cd1e22419b38
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-03-21 12:38:28 -0600

    added if statements

--- friendly-parakeet-main/friendly-parakeet-main/Develop/assets/script.js
+++ friendly-parakeet-main/friendly-parakeet-main/Develop/assets/script.js
@@ -17,13 +17,48 @@ function generatePassword() {
 
   var passwordlength = prompt("Password must be between 8-128 characters!");
 
-  if (passwordlength < 8){
+  if (passwordlength < 8) {
     alert("Password must have more than 7 characters!");
     return "";
   }
-  else (passwordlength > 128){
+  else if (passwordlength > 128) { 
     alert("Password must have less than 128 characters!");
     return"";
+  }
+
+  var lowercaseConfirm = confirm("Did you add a lowercase character?");
+
+  if (lowercaseConfirm) {
+    passwordText += lowercase;
+  }
+
+  var uppercaseConfirm = confirm("Did you add a uppercase character?");
+
+  if (uppercaseConfirm) {
+    passwordText += uppercase;
+  }
+
+  var numbersConfirm = confirm("Did you add a number?");
+
+  if (numbersConfirm) {
+    passwordText += numbers;
+  }
+
+  var specialConfirm = confirm("Did you add a special character?");
+
+  if (specialConfirm) {
+    passwordText += special;
+  }
+
+  for (var i=0; i < passwordlength; i++) {
+    password = passwordText[Math.floor(Math.random() * passwordText.length)]
+  }
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
   }
 }
 // Write password to the #password input
